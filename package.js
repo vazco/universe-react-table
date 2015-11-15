1c03cad5c84559cc28b4bd1f77dfc23e34d3ad7c
cristo-rabani <cristo.rabani@gmail.com>
2015-11-16 00:19:46 +0100

    nn

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'universe:react-table',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'A simple data tables, which allows you to show tabular data, and provides sorting, filtering',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/vazco/universe-react-table',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
-  api.use('universe:modules');
+  api.use('universe:modules@0.6.1');
 
   api.addFiles('reactable/lib/extract_data_from.import.jsx');
   api.addFiles('reactable/lib/filter_props_from.import.jsx');
